56165010430bc7ca10fa797879e7bff98a999499
FabrazJVargas <m.vargas.or10@gmail.com>
2024-06-13 00:43:28 -0500

    Actualizacion nocturna

--- src/components/CSubject.js
+++ src/components/CSubject.js
@@ -1,12 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import '../style/modal.css'
- 
+import React, { useState } from 'react';
 
 const CSubject = () => {
   const [showModal, setShowModal] = useState(false);
 
-  const openModal = (e) => {
-    e.preventDefault(); // Prevenir navegación predeterminada
+  const openModal = () => {
     setShowModal(true);
   };
 
@@ -14,47 +11,33 @@ const CSubject = () => {
     setShowModal(false);
   };
 
-  const handleKeyDown = (e) => {
-    if (e.key === 'Escape') {
-      closeModal();
-    }
-  };
-
-  useEffect(() => {
-    if (showModal) {
-      window.addEventListener('keydown', handleKeyDown);
-    } else {
-      window.removeEventListener('keydown', handleKeyDown);
-    }
-
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [showModal]);
-
   return (
     <div className="complementary-subjects">
       <i className="info-icon">ℹ️</i>
       <span>
         Puede consultar las materias complementarias aquí:
-        <a href="#" onClick={openModal}> Materias Complementarias</a>
+        <button className="btn btn-link" onClick={openModal}>Materias Complementarias</button>
       </span>
+
       {showModal && (
-      <div className="modal-overlay" onClick={closeModal}>
-        <div className="modal" onClick={(e) => e.stopPropagation()}>
-          <button className="close-btn" onClick={closeModal}>Cerrar</button>
-          <div className="custom-content">
-            <h2>Materias Complementarias</h2>
-            <p>Aquí puedes encontrar información adicional sobre las materias complementarias.</p>
-            <ul>
-              <li>Materia 1</li>
-              <li>Materia 2</li>
-              <li>Materia 3</li>
-            </ul>
+        <div className="modal" tabIndex="-1" style={{ display: 'block' }}>
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Materias Complementarias</h5>
+                <button type="button" className="btn-close" onClick={closeModal} aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <p>Aquí puedes encontrar información adicional sobre las materias complementarias.</p>
+            
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" onClick={closeModal}>Cerrar</button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    )}
+      )}
     </div>
   );
 };
