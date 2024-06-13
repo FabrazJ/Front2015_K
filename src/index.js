73d3c9ebe2b8680795f9310c583a37e7373aadc2
FabrazJVargas <m.vargas.or10@gmail.com>
2024-06-13 00:47:12 -0500

    Actualizacion

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Material from './components/Material';
- 
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
