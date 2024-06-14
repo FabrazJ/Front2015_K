83606278544fc3bf9dfd607ef26191b58fd5d34e
FabrazJVargas <m.vargas.or10@gmail.com>
2024-06-14 10:44:39 -0500

    Arreglo

--- src/components/subcomponents/mallas/Semestre.js
+++ src/components/subcomponents/mallas/Semestre.js
@@ -1,20 +1,42 @@
+// import React from 'react';
+// import Materia from './Materia';
+// import { Row, Col } from 'react-bootstrap';
+
+// const Semestre = ({ semestre, materias }) => {
+//     return (
+//         <div>
+//             <h2>Semestre {semestre}</h2>
+//             <Row>
+//                 {materias.map((materia, index) => (
+//                     <Col key={index}>
+//                         <Materia title={materia.title} credits={materia.credits} image={materia.image} />
+//                     </Col>
+//                 ))}
+//             </Row>
+//         </div>
+//     );
+// }
+
+// export default Semestre;
 import React from 'react';
-import Materia from './Materia';
-import { Row, Col } from 'react-bootstrap';
 
 const Semestre = ({ semestre, materias }) => {
+    if (!Array.isArray(materias)) {
+        return <div>No hay materias disponibles para el semestre {semestre}</div>;
+    }
+
     return (
         <div>
             <h2>Semestre {semestre}</h2>
-            <Row>
-                {materias.map((materia, index) => (
-                    <Col key={index}>
-                        <Materia title={materia.title} credits={materia.credits} image={materia.image} />
-                    </Col>
-                ))}
-            </Row>
+            {materias.map((materia, index) => (
+                <div key={index}>
+                    <img src={materia.image} alt={materia.title} />
+                    <h3>{materia.title}</h3>
+                    <p>Créditos: {materia.credits}</p>
+                </div>
+            ))}
         </div>
     );
-}
+};
 
 export default Semestre;
--- src/components/subcomponents/mallas/Semestres.js
+++ src/components/subcomponents/mallas/Semestres.js
@@ -1,20 +1,69 @@
-import React from 'react';
-import Materia from './Materia';
-import { Row, Col } from 'react-bootstrap';
+// import React from 'react';
+// import Semestre from './Semestre';
 
-const Semestre = ({ semestre, materias }) => {
+// const semestresData = [
+//     {
+//         semestre: 1,
+//         materias: [
+//             { title: 'Física: Mecánica', credits: 6, image: 'path/to/image1.png' },
+//             { title: 'Cálculo de una Variable', credits: 9, image: 'path/to/image2.png' },
+//             // Añade más materias según sea necesario
+//         ],
+//     },
+//     {
+//         semestre: 2,
+//         materias: [
+//             { title: 'Mineralogía', credits: 6, image: 'path/to/image3.png' },
+//             { title: 'Inglés I', credits: 5, image: 'path/to/image4.png' },
+//             // Añade más materias según sea necesario
+//         ],
+//     },
+//     // Añade más semestres según sea necesario
+// ];
+
+// const Semestres = () => {
+//     return (
+//         <div>
+//             {semestresData.map((data, index) => (
+//                 <Semestre key={index} semestre={data.semestre} materias={data.materias} />
+//             ))}
+//         </div>
+//     );
+// }
+
+// export default Semestres;
+
+import React from 'react';
+import Semestre from './Semestre';
+
+const semestresData = [
+    {
+        semestre: 1,
+        materias: [
+            { title: 'Física: Mecánica', credits: 6, image: 'path/to/image1.png' },
+            { title: 'Cálculo de una Variable', credits: 9, image: 'path/to/image2.png' },
+            // Añade más materias según sea necesario
+        ],
+    },
+    {
+        semestre: 2,
+        materias: [
+            { title: 'Mineralogía', credits: 6, image: 'path/to/image3.png' },
+            { title: 'Inglés I', credits: 5, image: 'path/to/image4.png' },
+            // Añade más materias según sea necesario
+        ],
+    },
+    // Añade más semestres según sea necesario
+];
+
+const Semestres = () => {
     return (
         <div>
-            <h2>Semestre {semestre}</h2>
-            <Row>
-                {materias.map((materia, index) => (
-                    <Col key={index}>
-                        <Materia title={materia.title} credits={materia.credits} image={materia.image} />
-                    </Col>
-                ))}
-            </Row>
+            {semestresData.map((data, index) => (
+                <Semestre key={index} semestre={data.semestre} materias={data.materias} />
+            ))}
         </div>
     );
 }
 
-export default Semestre;
+export default Semestres;
