c445e4bf9db80b77c37d5243cab7b0eeb3396a9a
FabrazJVargas <m.vargas.or10@gmail.com>
2024-06-14 10:28:37 -0500

    Intento 1

---
+++ src/components/subcomponents/mallas/Materia.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const Materia = ({ title, credits, image }) => {
+    return (
+        <Card style={{ width: '18rem', margin: '10px' }}>
+            <Card.Img variant="top" src={image} />
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                    Créditos: {credits}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default Materia;
