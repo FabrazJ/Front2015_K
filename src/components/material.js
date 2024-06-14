608edb1afaa7025a42bc8315cb03aacd8a1f646f
FabrazJVargas <m.vargas.or10@gmail.com>
2024-06-14 09:29:31 -0500

    Actualizacion de Malla

--- src/components/material.js
+++ src/components/material.js
@@ -7,14 +7,28 @@ import HorasSemestrales from './subcomponents/mallas/HorasSemestrales';
 import Niveles from './subcomponents/mallas/Niveles';
 // Styles
 import '../style/prueba.css'
+//Contenedor de prueba
+import { Container, Row, Col } from 'react-bootstrap';
+
 const Material = () => {
   return (
-    <div className="material-container">
+    <Container >
       <h1>Computación</h1>
       <CSubject />
-      <Itineraries/>
-      {/* <Malla /> */}
-    </div>
+      <Itineraries />
+
+      <Row>
+        <Col md={2}>
+          <Niveles />
+        </Col>
+        <Col md={8}>
+          <Semestres />
+        </Col>
+        <Col md={2}>
+          <HorasSemestrales />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
