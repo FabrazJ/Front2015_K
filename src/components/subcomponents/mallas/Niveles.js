1584d57bc9072c2ec5a1cadc222f7846b3c8ece6
FabrazJVargas <m.vargas.or10@gmail.com>
2024-06-24 08:53:23 -0500

    Actualizacion

--- src/components/subcomponents/mallas/Niveles.js
+++ src/components/subcomponents/mallas/Niveles.js
@@ -1,22 +1,39 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Table } from 'react-bootstrap';
 
 const Niveles = () => {
     return (
         <Card style={{ border: 'none' }}>
             <Card.Body>
                 <Card.Title>Niveles</Card.Title>
-                <Card.Text>
-                <br/>
-                    NIVEL 100 - I<br/><br/><br/>
-                    NIVEL 100 - II<br/><br/><br/>
-                    NIVEL 200 - I<br/><br/><br/>
-                    NIVEL 200 - II<br/><br/><br/>
-                    NIVEL 300 - I<br/><br/><br/>
-                    NIVEL 300 - II<br/><br/><br/>
-                    NIVEL 400 - I<br/><br/><br/>
-                    NIVEL 400 - II
-                </Card.Text>
+                <Table striped bordered hover>
+                    <tbody>
+                        <tr>
+                            <td>NIVEL 100 - I</td>
+                        </tr>
+                        <tr>
+                            <td>NIVEL 100 - II</td>
+                        </tr>
+                        <tr>
+                            <td>NIVEL 200 - I</td>
+                        </tr>
+                        <tr>
+                            <td>NIVEL 200 - II</td>
+                        </tr>
+                        <tr>
+                            <td>NIVEL 300 - I</td>
+                        </tr>
+                        <tr>
+                            <td>NIVEL 300 - II</td>
+                        </tr>
+                        <tr>
+                            <td>NIVEL 400 - I</td>
+                        </tr>
+                        <tr>
+                            <td>NIVEL 400 - II</td>
+                        </tr>
+                    </tbody>
+                </Table>
             </Card.Body>
         </Card>
     );
